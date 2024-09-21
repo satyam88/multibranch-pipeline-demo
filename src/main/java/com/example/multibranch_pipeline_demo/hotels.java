6d93075439224b45dacbfb52a02df270fda97fe2
Satyam Pandey <satyampandey88@gmail.com>
2024-09-21 08:24:59 +0530

    hello

--- src/main/java/com/example/multibranch_pipeline_demo/hotels.java
+++ src/main/java/com/example/multibranch_pipeline_demo/hotels.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HotelController {
-
+public class hotels {
     @GetMapping("/hotels")
-    public String getHotels() {
-        return "List of hotels";
-    }
+    public String getData() {return  "Please book hotel from mmt 75% discount in all india" ; }
 }
